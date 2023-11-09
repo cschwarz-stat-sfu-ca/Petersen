e2ebb2f40de4251070b3499fa127d483a15fc676
cschwarz-stat-sfu-cae <cschwarz.stat.sfu.ca@gmail.com>
2023-11-09 10:22:02 -0800

    CRAN submission issues

--- man-roxygen/param.quietly.R
+++ man-roxygen/param.quietly.R
@@ -1,4 +1,4 @@
-#' @param Suppress all console messages that occur during the fit. This includes the progress bar
+#' @param quietly Suppress all console messages that occur during the fit. This includes the progress bar
 #' when a model that requires MCMC is fit (*LP_BTSPAS_fit_Diag* and *LP_BTSPAS_fit_NonDiag*), or
 #' a trace of the likelihood during the fit (*LP_SPAS_fit*).
 #'
